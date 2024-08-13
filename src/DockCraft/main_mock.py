cfe0ffd679f3538a2ac0a12638fb46a16fd9c2c6
Stefan Lacher <stefan.lacher@alumni.uni-graz.at>
2024-08-13 12:41:08 +0200

    feat: added initial code files for DockCraft

---
+++ src/DockCraft/main_mock.py
@@ -0,0 +1,6 @@
+"""this is the main module for the DockCraft application"""
+
+
+def main() -> None:
+    """this is the main function for the DockCraft application"""
+    print("DockCraft is running!")
---
+++ src/tests/unit/test_mock.py
@@ -0,0 +1,14 @@
+"""this is a mock test"""
+
+from src.DockCraft.main_mock import main
+
+
+def test_mock() -> None:
+    """this is a test function for the mock function"""
+    assert True
+
+
+def test_main_mock() -> None:
+    """this is a test function for the main mock function"""
+    main()
+    assert True
