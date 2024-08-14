cc69f16c58c0a22ee273d48bb38bca46ca8a8139
Stefan Lacher <stefan.lacher@alumni.uni-graz.at>
2024-08-14 08:37:57 +0200

    mock-testing: remove all mocks

--- src/DockCraft/main_mock.py
+++
@@ -1,6 +0,0 @@
-"""this is the main module for the DockCraft application"""
-
-
-def main() -> None:
-    """this is the main function for the DockCraft application"""
-    print("DockCraft is running!")
