fa3b8baffb81c0b65028ab7b46503418a69fc9b3
Stefan Lacher <stefan.lacher@alumni.uni-graz.at>
2024-08-14 12:51:22 +0200

    Dockerfile Reader: add logging

--- src/DockCraft/general/dockcraft_reader.py
+++ src/DockCraft/general/dockcraft_reader.py
@@ -3,6 +3,7 @@ this module is responsible for reading the dockerfile
 and returning the data in a usable format
 """
 
+import logging
 import os
 from typing import List
 
@@ -77,7 +78,7 @@ class DockerfileReader:
         None
         """
         for line in self.content:
-            print(line, end="")
+            logging.debug(line)
 
 
 if __name__ == "__main__":  # pragma: no cover
